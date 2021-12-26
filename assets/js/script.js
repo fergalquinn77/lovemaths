212044f7c39f05e4cf60e19702d6b9dd4c970c1e
fergalquinn77 <fergal@bourbon.ie>
2021-12-26 10:57:29 +0000

    add code to create two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,9 +19,19 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 })
 
+/**
+*The main game "loop" called when the script is first loaded
+*and after the users answer has been processed 
+*/
 function runGame(){
 
+//Generate two random numbers between 1 & 25
+    let num1=Math.floor(Math.random()*25)+1;
+    let num2=Math.floor(Math.random()*25)+1;
+
 }
+
+runGame();
 
 function checkAnswer(){
 
