47525e72ece21a4e6d137a722b11af99319b2d41
fergalquinn77 <fergal@bourbon.ie>
2021-12-26 14:49:49 +0000

    ad code for division - challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,6 +101,9 @@ function calculateCorrectAnswer(){
     else if (operator === '-'){
         return [operand1 - operand2, "subtract"];
     }
+    else if (operator === '/'){
+        return [operand1 / operand2, "division"];
+    }
     else
     {
         alert(`unimplented operator ${operator}`);
@@ -141,6 +144,7 @@ function displaySubtractQuestion(operand1, operand2){
     
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+
     document.getElementById("operator").textContent = "-";
 
 }
@@ -150,5 +154,14 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2){
+
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1-operand1%operand2 : operand2-operand2%operand1;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2: operand1;
+    document.getElementById("operator").textContent = "/";
 
 }
